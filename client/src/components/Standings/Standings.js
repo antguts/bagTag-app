897f1708f7c4f9074ea1c200d1bff981cad3a60c
antguts <antgut035@gmail.com>
2021-01-12 22:48:02 -0500

    Created and added page mockups to assets folder

---
+++ client/src/components/Standings/Standings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Standings =()=>{
+
+
+    return(
+        <div>
+            <h1>Standings</h1>
+        </div>
+    );
+};
+
+export default Standings;
